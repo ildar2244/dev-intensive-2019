165047654a41ae39b6213ba4d3e168775c722202
ildar2244 <ildar2244@gmail.com>
2019-10-25 15:47:26 +0300

    Прогон решений через тесты (hometask_2)

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -13,6 +13,7 @@ abstract class BaseMessage(
     abstract fun formatMessage(): String
 
     companion object AbstractFactory {
+        /* мой вариант. тест проходит BaseMessageTest.makeMessageTest()
         var lastId = -1
 
         fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text",
@@ -20,8 +21,23 @@ abstract class BaseMessage(
             lastId++
 
             return when(type) {
-                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String)
-                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String)
+                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+            }
+        } */
+
+        /* Другой вариант решения от лектора */
+        var lastId = 0
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type:String = "text",
+                        payload: Any, isIncoming: Boolean = false):BaseMessage{
+            return when(type){
+                "image"-> ImageMessage("${lastId++}", from, chat, date = date,
+                    image = payload as String, isIncoming = isIncoming)
+                "text" -> TextMessage("${lastId++}", from, chat, date = date,
+                    text = payload as String, isIncoming = isIncoming)
+
+                else-> if ("image" == payload || "text" == payload) makeMessage(from, chat, date,
+                    payload, type, isIncoming) else throw IllegalArgumentException()
             }
         }
     }
--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -29,13 +29,41 @@ data class User(
     }
 
     companion object Factory {
-        private var lastId: Int = -1
+        private var lastId: Int = 0
 
         fun makeUser(fullName: String?): User{
-            lastId++
             val (firstName, lastName) = Utils.parseFullName(fullName)
-            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+            return User(id = "${lastId++}", firstName = firstName, lastName = lastName)
         }
+    }
+
+    class Builder {
+        var id : String = ""
+        var firstName : String? = null
+        var lastName : String? = null
+        var avatar : String? = null
+        var rating : Int = 0
+        var respect : Int = 0
+        var lastVisit : Date? = Date()
+        var isOnline : Boolean = false
+
+        fun id(id: String) = apply { this.id = id }
+
+        fun firstName(firstName: String?) = apply { this.firstName = firstName }
+
+        fun lastName(lastName: String?)= apply { this.lastName = lastName }
+
+        fun avatar(avatar: String?)= apply { this.avatar = avatar }
+
+        fun rating(rating: Int)= apply { this.rating = rating }
+
+        fun respect(respect: Int)= apply { this.respect = respect }
+
+        fun lastVisit(lastVisit: Date?)= apply { this.lastVisit = lastVisit }
+
+        fun isOnline(isOnline: Boolean)= apply { this.isOnline = isOnline }
+
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
 
     fun printMe() = println(
